f926b345a749889e916623ca736439ae06257c71
q40603 <q40603@gmail.com>
2020-05-06 22:56:30 +0800

    all

--- hw2/scan.py
+++ hw2/scan.py
@@ -1,12 +1,12 @@
-import scapy.all as scapy
+from scapy.all import all
 import sys
 
 
 def scan(ip):
-    arp_request = scapy.ARP(pdst=ip)
-    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request = ARP(pdst=ip)
+    broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    answered_list = srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
     client_list = []
     for element in answered_list:
