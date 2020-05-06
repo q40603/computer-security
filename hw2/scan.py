d1cf46940e295df43b9aef96a65e93f6eb17e018
q40603 <q40603@gmail.com>
2020-05-06 22:11:51 +0800

    ip

--- hw2/scan.py
+++ hw2/scan.py
@@ -3,7 +3,7 @@ import sys
 
 
 def scan(ip):
-    arp_request = scapy.ARP(pdst=ip)
+    arp_request = scapy.ARP()
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
